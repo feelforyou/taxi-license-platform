a82ec611d8d625e7fcbb08196cedb707c1789aa5
feelforyou <khmaladze.2012@gmail.com>
2023-08-24 23:13:53 +0400

    manual chunk approach has been added invite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Define manual chunks for vendor libraries
+        manualChunks: {
+          // 'vendor' chunk will include the specified libraries
+          'vendor': ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
+  }
+});
